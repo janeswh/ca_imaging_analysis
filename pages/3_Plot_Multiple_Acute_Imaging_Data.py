d1b039a8f38fffb835ad1c19da8be2e6cdeec3b4
Jane Huang <62308312+janeswh@users.noreply.github.com>
2023-02-14 18:48:28 -0500

    fixed plotting logic

--- pages/3_Plot_Multiple_Acute_Imaging_Data.py
+++ pages/3_Plot_Multiple_Acute_Imaging_Data.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from collections import defaultdict
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -42,11 +43,21 @@ def initialize_states():
         st.session_state.sig_data = False
     if "sig_odors" not in st.session_state:
         st.session_state.sig_odors = False
+    if "nosig_exps" not in st.session_state:
+        st.session_state.no_sig_exps = False
 
     if "plots_list" not in st.session_state:
         st.session_state.plots_list = False
     if "selected_odor" not in st.session_state:
         st.session_state.selected_odor = False
+
+    # measures to plot
+    st.session_state.measures = [
+        "Blank-subtracted DeltaF/F(%)",
+        "Area under curve",
+        "Latency (s)",
+        "Time to peak (s)",
+    ]
 
     st.session_state.files = st.file_uploader(
         label="Choose files",
@@ -130,7 +141,11 @@ def main():
         if st.button("Load data"):
             st.session_state.load_data = True
 
-            nosig_exps, odors_list, st.session_state.sig_data = import_data()
+            (
+                st.session_state.nosig_exps,
+                odors_list,
+                st.session_state.sig_data,
+            ) = import_data()
 
             st.info(
                 f"Response data loaded successfully for "
@@ -142,7 +157,7 @@ def main():
                 odor for sublist in odors_list for odor in sublist
             ]
 
-            if len(nosig_exps) == len(st.session_state.files):
+            if len(st.session_state.nosig_exps) == len(st.session_state.files):
                 st.error(
                     "None of the uploaded experiments have significant "
                     " odor responses. Please upload data for experiments with "
@@ -156,14 +171,14 @@ def main():
                 )
                 st.session_state.sig_odors.sort()
 
-            # # if load data is clicked again, doesn't display plots/slider
-            # st.session_state.plots_list = False
+            # if load data is clicked again, doesn't display plots/slider
+            st.session_state.plots_list = False
 
-            # if data has been loaded, always show plotting buttons
-
+        # if data has been loaded, always show plotting buttons
         if st.session_state.load_data:
             if st.button("Plot data"):
-                plots_list = {}
+                # plots_list = {}
+                plots_list = defaultdict(dict)
 
                 line_color_scale = [
                     "#D00000",
@@ -196,23 +211,15 @@ def main():
                         if odor in st.session_state.sig_data[experiment]:
                             sig_odor_exps.append(experiment)
 
-                    odor_fig = make_subplots(
-                        rows=1,
-                        cols=4,
-                        horizontal_spacing=0.1,
-                        x_title="Experiment ID",
-                    )
+                    for measure in st.session_state.measures:
+                        measure_fig = go.Figure()
 
-                    for exp_ct, sig_experiment in enumerate(sig_odor_exps):
-                        exp_odor_df = st.session_state.sig_data[
-                            sig_experiment
-                        ][odor]
+                        for exp_ct, sig_experiment in enumerate(sig_odor_exps):
+                            exp_odor_df = st.session_state.sig_data[
+                                sig_experiment
+                            ][odor]
 
-                        # plots one measurement per subplot
-                        for measure_ct, measure in enumerate(
-                            exp_odor_df.index.values
-                        ):
-                            odor_fig.add_trace(
+                            measure_fig.add_trace(
                                 go.Box(
                                     x=[sig_experiment]
                                     * len(exp_odor_df.loc[measure].values),
@@ -231,8 +238,6 @@ def main():
                                     ),
                                     name=sig_experiment,
                                 ),
-                                row=1,
-                                col=measure_ct + 1,
                             )
 
                             # add horizontal line for mean
@@ -242,7 +247,7 @@ def main():
 
                             start = (1 / len(sig_odor_exps)) / 4
 
-                            odor_fig.add_shape(
+                            measure_fig.add_shape(
                                 type="line",
                                 line=dict(
                                     color=line_color_scale[exp_ct],
@@ -262,62 +267,69 @@ def main():
                                 # x1=start + (exp_ct * line_width) + line_width,
                                 y0=exp_odor_df.loc[measure].values.mean(),
                                 y1=exp_odor_df.loc[measure].values.mean(),
-                                row=1,
-                                col=measure_ct + 1,
                             )
 
                             #  below is code from stack overflow to hide duplicate legends
                             names = set()
-                            odor_fig.for_each_trace(
+                            measure_fig.for_each_trace(
                                 lambda trace: trace.update(showlegend=False)
                                 if (trace.name in names)
                                 else names.add(trace.name)
                             )
 
-                            odor_fig.update_xaxes(showticklabels=False)
+                            measure_fig.update_xaxes(showticklabels=False)
 
-                            odor_fig.update_yaxes(
+                            measure_fig.update_yaxes(
                                 title_text=measure,
-                                rangemode="tozero",
-                                row=1,
-                                col=measure_ct + 1,
                             )
 
-                            # if measure == "Latency (s)":
-                            #     odor_fig.update_yaxes(
-                            #         rangemode="tozero",
-                            #         row=1,
-                            #         col=4,
-                            #     )
-                            odor_fig.update_layout(
+                            if measure == "Time to peak (s)":
+                                measure_fig.update_yaxes(
+                                    rangemode="tozero",
+                                )
+
+                            measure_fig.update_layout(
                                 boxgap=0.4,
                                 title={
-                                    "text": odor,
+                                    "text": measure,
                                     "x": 0.4,
                                     "xanchor": "center",
                                 },
                                 legend_title_text="Experiment ID<br />",
+                                showlegend=True,
                             )
+
+                            plots_list[odor][measure] = measure_fig
+
                     odor_bar.set_description(f"Plotting {odor}", refresh=True)
-                    plots_list[odor] = odor_fig
+                    # plots_list[odor] = odor_fig
 
                 st.info("All plots generated.")
+                if len(st.session_state.nosig_exps) != 0:
+                    st.warning(
+                        "No plots have been generated for the "
+                        "following experiments due to the lack of significant "
+                        "odor responses: \n"
+                        f"{st.session_state.nosig_exps}"
+                    )
+
                 st.session_state.plots_list = plots_list
 
             # display slider and plots if plots have already been generated
             # even if Plot data isn't clicked again
             if st.session_state.plots_list:
                 st.session_state.selected_odor = st.select_slider(
-                    "Select odor number to display its " "corresponding plot:",
+                    "Select odor number to display its corresponding plots:",
                     options=st.session_state.sig_odors,
                 )
 
                 if st.session_state.selected_odor:
-                    st.plotly_chart(
-                        st.session_state.plots_list[
-                            st.session_state.selected_odor
-                        ]
-                    )
+                    for measure in st.session_state.measures:
+                        st.plotly_chart(
+                            st.session_state.plots_list[
+                                st.session_state.selected_odor
+                            ][measure]
+                        )
 
 
 if __name__ == "__main__":
