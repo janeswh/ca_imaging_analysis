7b546eff2bc90a6a56ac29a8d277352f95b7bfb9
Jane Huang <62308312+janeswh@users.noreply.github.com>
2023-07-03 16:01:18 -0400

    moved CHANGELOG.md to app/ folder

--- app/pages/5_Changelog.py
+++ app/pages/5_Changelog.py
@@ -5,7 +5,7 @@ import os
 
 def show_changelog():
     # suppose that ChangeLog.md is located at the same folder as Streamlit app
-    path = Path(__file__).parents[2]
+    path = Path(__file__).parents[1]
     with open(os.path.join(path, "CHANGELOG.md"), "r", encoding="utf-8") as f:
         lines = f.readlines()
 
--- run_docker_scripts/run_docker.sh
+++ run_docker_scripts/run_docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Allow X server connection
 xhost +local:root
+docker pull janeswh/ca_imaging_analysis
+
 docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     --env="QT_X11_NO_MITSHM=1" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /home/jhuang:/app/local_computer \
+    -v /home/jhuang:/app_dir/local_computer \
     -p 8501:8501 \
-    roi_analysis
+    janeswh/ca_imaging_analysis
 # Disallow X server connection
 xhost -local:root
+
