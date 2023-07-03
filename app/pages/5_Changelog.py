88852d40370113d7a2d52116595f2b0bdf2d0bbb
Jane Huang <62308312+janeswh@users.noreply.github.com>
2023-07-03 15:26:54 -0400

    fixed changelog page location

--- app/pages/5_Changelog.py
+++ app/pages/5_Changelog.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def show_changelog():
     # suppose that ChangeLog.md is located at the same folder as Streamlit app
-    with open("../CHANGELOG.md", "r", encoding="utf-8") as f:
+    with open("../..CHANGELOG.md", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
     # display entries
