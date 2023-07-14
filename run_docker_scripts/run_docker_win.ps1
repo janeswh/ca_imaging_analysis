fae9b578311698769d63f4fe2deae575b39f9a0c
Jane Huang <62308312+janeswh@users.noreply.github.com>
2023-07-13 23:06:24 -0400

    fix: fixed directory in run_docker_win.ps1

--- run_docker_scripts/run_docker_win.ps1
+++ run_docker_scripts/run_docker_win.ps1
@@ -1,5 +1,5 @@
 # gets directory where all experiment folders reside, assuming the docker img folder is in the same directory
-# $exp_dir = Split-Path -Path $PSScriptRoot -Parent
+$exp_dir = Split-Path -Path $PSScriptRoot -Parent
 
 # load docker image
 docker pull janeswh/ca_imaging_analysis
@@ -13,6 +13,7 @@ $Env:IPADDRESS = ((Test-Connection -ComputerName $Env:ComputerName -Count 1).IPV
 # cd ..
 
 # changed IPADDRESS variable
-docker run --rm -it -e DISPLAY=$Env:IPADDRESS -p:8501:8501 -v "$(pwd):/app_dir/local_drive" janeswh/ca_imaging_analysis
+# docker run --rm -it -e DISPLAY=$Env:IPADDRESS -p:8501:8501 -v "$(pwd):/app_dir/local_drive" janeswh/ca_imaging_analysis
+docker run --rm -it -e DISPLAY=$Env:IPADDRESS -p:8501:8501 -v "${exp_dir}:/app_dir/local_drive" janeswh/ca_imaging_analysis
 
 Read-Host -Prompt "Press enter to exit"
